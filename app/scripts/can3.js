bf50a6af5a7f385f66b043d9acdeca24fb7d363e
timotius02 <timsitorus1@gmail.com>
2016-11-06 09:40:28 -0500

    everything

---
+++ app/scripts/can3.js
@@ -0,0 +1,119 @@
+(function() {
+    var canvas, ctx, flag = false,
+    prevX = 0,
+    currX = 0,
+    prevY = 0,
+    currY = 0,
+    dot_flag = false;
+
+     var x = "black",
+         y = 4;
+    
+    function init() {
+        canvas = document.getElementById('can3');
+       // canvas1 = document.getElementById('can1');
+        ctx = canvas.getContext("2d");
+        w = canvas.width;
+        h = canvas.height;
+    
+        canvas.addEventListener("mousemove", function (e) {
+            findxy('move', e)
+        }, false);
+        canvas.addEventListener("mousedown", function (e) {
+            findxy('down', e)
+        }, false);
+        canvas.addEventListener("mouseup", function (e) {
+            findxy('up', e)
+        }, false);
+        canvas.addEventListener("mouseout", function (e) {
+            findxy('out', e)
+        }, false);
+    }
+    
+    function color(obj) {
+         // x = obj.value;
+        switch (obj.value) {
+            case "0, 256, 0":
+                x = "green";
+                break;
+            case "0, 0, 256":
+                 x = "blue";
+                 break;
+             case "256, 0, 0":
+                x = "red";
+                break;
+             case "256, 256, 0":
+                 x = "yellow";
+                 break;
+            // case "orange":
+            //     x = "orange";
+            //     break;
+             case "0, 0, 0":
+                 x = "black";
+                 break;
+             case "256, 256, 256":
+                 x = "white";
+                 break;
+         }
+        y = 4;
+    }
+    
+    function draw() {
+        ctx.beginPath();
+        ctx.moveTo(prevX, prevY);
+        ctx.lineTo(currX, currY);
+        ctx.strokeStyle = x;
+        ctx.lineWidth = y;
+        ctx.stroke();
+        ctx.closePath();
+    }
+    
+    function erase() {
+        var m = confirm("Want to clear");
+        if (m) {
+            ctx.clearRect(0, 0, w, h);
+            document.getElementById("canvasimg").style.display = "none";
+        }
+    }
+    
+    function save() {
+        document.getElementById("canvasimg").style.border = "2px solid";
+        var dataURL = canvas.toDataURL();
+        document.getElementById("canvasimg").src = dataURL;
+        document.getElementById("canvasimg").style.display = "inline";
+    }
+    
+    function findxy(res, e) {
+        if (res == 'down') {
+            prevX = currX;
+            prevY = currY;
+            currX = e.clientX - canvas.offsetLeft;
+            currY = e.clientY - canvas.offsetTop;
+    
+            flag = true;
+            dot_flag = true;
+            if (dot_flag) {
+                ctx.beginPath();
+                ctx.fillStyle = x;
+                ctx.fillRect(currX, currY, 2, 2);
+                ctx.closePath();
+                dot_flag = false;
+            }
+        }
+        if (res == 'up' || res == "out") {
+            flag = false;
+        }
+        if (res == 'move') {
+            if (flag) {
+                prevX = currX;
+                prevY = currY;
+                currX = e.clientX - canvas.offsetLeft;
+                currY = e.clientY - canvas.offsetTop;
+                draw();
+            }
+        }
+    }
+
+    init();
+})();
+
---
+++ app/scripts/main2.js
@@ -0,0 +1,87 @@
+var blocklyArea = document.getElementById('blocklyArea');
+var blocklyDiv = document.getElementById('blocklyDiv');
+var workspace = Blockly.inject(blocklyDiv,
+    {toolbox: document.getElementById('toolbox')});
+var onresize = function(e) {
+  // Compute the absolute coordinates and dimensions of blocklyArea.
+  var element = blocklyArea;
+  var x = 0;
+  var y = 0;
+  do {
+    x += element.offsetLeft;
+    y += element.offsetTop;
+    element = element.offsetParent;
+  } while (element);
+  // Position blocklyDiv over blocklyArea.
+  blocklyDiv.style.left = x + 'px';
+  blocklyDiv.style.top = y + 'px';
+  blocklyDiv.style.width = blocklyArea.offsetWidth + 'px';
+  blocklyDiv.style.height = blocklyArea.offsetHeight + 'px';
+
+  function myUpdateFunction(event) {
+    var code = Blockly.JavaScript.workspaceToCode(workspace);
+    console.log(code);
+    eval(code);
+  }
+  workspace.addChangeListener(myUpdateFunction);
+};
+window.addEventListener('resize', onresize, false);
+onresize();
+Blockly.svgResize(workspace);
+
+var x = "black",
+         y = 4;
+function set_color(red, green, blue) {
+
+} 
+// function set_clothes(clothes, selection) {
+
+  
+//   switch (clothes) {
+//     case "Outfit":
+//       $('#barebody').show();
+//       if (selection === 'hacker') {
+//         $('.outfit').hide();
+//         $('#barebody').hide();
+//         $('#hacker').show();
+//       }
+//       else if (selection === 'firefighter') {
+//         $('.outfit').hide();
+//         $('#barebody').hide();
+//         $('#hacker').show();
+//         $('#firefighter').show();
+//       }
+//       else if (selection === 'doctor') {
+//         $('.outfit').hide();
+//         $('#barebody').hide();
+//         $('#hacker').show();
+//         $('#doctor').show();
+//       }
+//       else if (selection === 'business_suit' || selection === 'chef' || selection === 'astronaut') {
+//         $('.outfit').hide();
+//         $('#barebody').show();
+//         $('#' + selection).show();
+//       }
+//       break;
+//     case "Hat":
+
+//         $('.hat').hide();
+
+//       if (selection === 'firefighter_hat' || selection === 'chef_hat' || selection === 'astronaut_hat') {
+//         $('.hat').hide();
+//         $('#' + selection).show();
+//       }
+//       break;
+//     case "Accessory":
+
+//         $('.accessory').hide();
+      
+//       if (selection === 'surgeon_mask') {
+//         $('accessory').hide();
+//         $('#' + selection).show();
+//       }
+      
+//       break;
+//   }
+// }
+
